ffa88dccf936e2cd9d32e94f943d08d46e93bf3f
librasaur <lizzie.briggs10@gmail.com>
2022-03-09 14:54:52 +0000

    Added rotation to movement

--- FYP_DOD/Assets/Scripts/Systems/MovableSystem.cs
+++ FYP_DOD/Assets/Scripts/Systems/MovableSystem.cs
@@ -1,6 +1,8 @@
 using Components;
 using Unity.Entities;
+using Unity.Mathematics;
 using Unity.Transforms;
+using UnityEngine;
 
 namespace Systems
 {
@@ -10,9 +12,12 @@ namespace Systems
         {
             float dt = Time.DeltaTime;
             
-            Entities.ForEach((ref Movable move, ref Translation trans) =>
+            Entities.ForEach((ref Movable move, ref Translation trans, ref Rotation rot) =>
             {
                 trans.Value += move.speed * move.direction * dt;
+                
+                if (!move.direction.Equals(float3.zero)) 
+                    rot.Value = Quaternion.Slerp(rot.Value, Quaternion.LookRotation(move.direction), 0.15f);
             }).Schedule();
         }
     }
